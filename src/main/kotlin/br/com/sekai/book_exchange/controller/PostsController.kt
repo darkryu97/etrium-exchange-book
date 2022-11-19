52f5c23a765fe6582b60914d385984c4ad484d4a
FelipeSekai <felippe10_@hotmail.com>
2022-11-19 15:05:15 -0300

    migrate db para postgres

--- src/main/kotlin/br/com/sekai/book_exchange/controller/PostsController.kt
+++ src/main/kotlin/br/com/sekai/book_exchange/controller/PostsController.kt
@@ -5,14 +5,9 @@ import br.com.sekai.book_exchange.data.vo.v1.PostsVO
 import br.com.sekai.book_exchange.service.PostService
 import br.com.sekai.book_exchange.uitl.MediaType
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
-@CrossOrigin(origins = ["http://127.0.0.1:5173/", "https://book-exchange-1kvv.vercel.app/", "http://127.0.0.1:8080/"])
+
 @RestController
 @RequestMapping("/api/posts/v1")
 class PostsController {
